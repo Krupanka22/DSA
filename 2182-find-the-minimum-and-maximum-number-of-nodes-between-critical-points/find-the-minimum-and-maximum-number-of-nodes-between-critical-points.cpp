844c94d29e2c151f794b10d334281095302dcdac
KRUPANKA R <166852949+Krupanka22@users.noreply.github.com>
2026-08-31 08:19:54 +0530

    Time: 3 ms (67.35%) | Memory: 128.6 MB (7.49%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,54 +1,42 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        vector<int> temp ;
-
-        if(head==NULL || head->next==NULL || head->next->next==NULL) 
-            return {-1 , -1};
         
-        int m=INT_MAX ;
-        int ma=0;
-        
-        int cnt=2;
-        ListNode *prev = head;
-        ListNode *curr = head->next ;
+        vector<int> criticalPoints;
 
-        while(curr->next !=NULL){
-            if(curr->val < prev->val && curr->val<curr->next->val){
-                temp.push_back(cnt);
+        ListNode* prev = head;
+        ListNode* curr = head->next;
+
+        int position = 2;
+
+        while (curr != NULL && curr->next != NULL) {
+
+            if ((curr->val > prev->val && curr->val > curr->next->val) ||
+                (curr->val < prev->val && curr->val < curr->next->val)) {
+
+                criticalPoints.push_back(position);
             }
-            else if(curr->val > prev->val && curr->val > curr->next->val){
-                temp.push_back(cnt);
-            }
-           prev = curr;
+
+            prev = curr;
             curr = curr->next;
-
-            cnt++ ;
-
-            
+            position++;
         }
 
-         if (temp.size() < 2)
+        if (criticalPoints.size() < 2)
             return {-1, -1};
 
-        for(int i=temp.size()-1 ; i>0 ; i--){
-            int dis= temp[i]-temp[i-1];
-            m=min(dis , m);
+        int minDistance = INT_MAX;
+
+        for (int i = 1; i < criticalPoints.size(); i++) {
+            minDistance = min(
+                minDistance,
+                criticalPoints[i] - criticalPoints[i - 1]
+            );
         }
 
-        ma = temp[temp.size() - 1] - temp[0];
+        int maxDistance =
+            criticalPoints.back() - criticalPoints.front();
 
-        return {m , ma};
-        
+        return {minDistance, maxDistance};
     }
 };
