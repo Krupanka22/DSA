3fd87c9ba5116666e8d9a4d95db3fed2e85d7a67
KRUPANKA R <166852949+Krupanka22@users.noreply.github.com>
2026-09-04 19:48:21 +0530

    Time: 29 ms (40.99%) | Memory: 15.9 MB (99.71%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,53 +1,38 @@
 class Solution {
 public:
 
-    void dfs(int i, int j, vector<vector<char>>& grid) {
+    void dfs(int i , int j , vector<vector<char>>& grid){
+        int n= grid.size();
+        int m= grid[0].size();
 
-        int n = grid.size();
-        int m = grid[0].size();
+        if(i < 0 || i >= n || j < 0 || j >= m) return ;
 
-        
-        if (i < 0 || i >= n || j < 0 || j >= m) {
-            return;
-        }
+        if(grid[i][j]=='0') return ;
 
-        
-        if (grid[i][j] == '0') {
-            return;
-        }
-
-    
         grid[i][j] = '0';
 
-        
-        dfs(i + 1, j, grid); 
-        dfs(i - 1, j, grid); 
-        dfs(i, j + 1, grid); 
-        dfs(i, j - 1, grid); 
+        dfs(i+1 , j , grid);
+        dfs(i-1 , j , grid);
+        dfs(i , j+1 , grid);
+        dfs(i , j-1 , grid);
     }
 
+
     int numIslands(vector<vector<char>>& grid) {
+        int n= grid.size();
+        int m= grid[0].size();
+        int count=0;
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<m ; j++){
+                if(grid[i][j] == '1'){
+                    dfs(i,j,grid);
 
-        int n = grid.size();
-        int m = grid[0].size();
-
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            for (int j = 0; j < m; j++) {
-
-                if (grid[i][j] == '1') {
-
-               
                     count++;
 
-                
-                    dfs(i, j, grid);
                 }
             }
         }
 
-        return count;
+        return count ;
     }
 };
