782ca71ddde0c51d51c28f5917b462685b7de9e1
KRUPANKA R <166852949+Krupanka22@users.noreply.github.com>
2026-09-05 11:52:42 +0530

    Time: 4 ms (89.30%) | Memory: 202.6 MB (76.32%) - LeetSync

---
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<int> suffixMin(n);
+
+        suffixMin[n - 1] = nums[n - 1];
+
+        for(int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = min(nums[i], suffixMin[i + 1]);
+        }
+
+        int maximum = INT_MIN;
+
+        for(int i = 0; i < n; i++) {
+            maximum = max(maximum, nums[i]);
+
+            if(maximum - suffixMin[i] <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
