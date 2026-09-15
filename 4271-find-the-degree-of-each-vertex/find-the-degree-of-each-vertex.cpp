8669004c82200b39429307829ec8d9f4a804132f
KRUPANKA R <166852949+Krupanka22@users.noreply.github.com>
2026-09-15 22:48:41 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (43.07%) - LeetSync

--- 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.cpp
+++ 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     vector<int> findDegrees(vector<vector<int>>& matrix) {
-        vector<int> ans;
+        int n = matrix.size();
+        vector<int> ans(n, 0);
         for(int i=0 ; i<matrix.size() ; i++){
-            int count = 0 ;
-            for(int j=0 ; j<matrix[0].size() ; j++){
+            for(int j=i ; j<matrix[0].size() ; j++){
                 if(matrix[i][j]==1){
-                    count++;
+                    ans[i]++;
+                    ans[j]++;
                 }
             }
-
-            ans.push_back(count);
         }
 
         return ans ;
